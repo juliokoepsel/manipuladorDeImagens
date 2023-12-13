0a3a2733d74b16880c5defd7e16d20a96b4a4d38
JulioKoepsel <julio.koepsel@gmail.com>
2023-12-13 04:36:53 -0300

    Update #2

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"manipuladorDeImagens","l":"ManipuladorImagem.Builder"},{"p":"manipuladorDeImagens","l":"ImagemPrototype"},{"p":"manipuladorDeImagens","l":"ManipuladorImagem"}];updateSearchResults();
